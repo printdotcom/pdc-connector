8d3043441a3c61f944d544fecbccc50d4c0019d1
tijmen <tijmenbruggeman@gmail.com>
2024-11-03 13:42:06 +0100

    Change staging URL to .stg.

--- admin/partials/pdc-connector-admin-section-environment.php
+++ admin/partials/pdc-connector-admin-section-environment.php
@@ -2,11 +2,11 @@
 <?php 
 $env = get_option($this->plugin_name . '-env_baseurl');
 $prodchecked = $env == 'https://api.print.com/' ? 'checked' : '';
-$stgchecked = $env == 'https://api.print.beer/' ? 'checked' : '';
+$stgchecked = $env == 'https://api.stg.print.com/' ? 'checked' : '';
 ?>
 <div class="environment">
     <input type="radio" <?= $prodchecked; ?> name="<?= $this->plugin_name; ?>-env_baseurl" value="https://api.print.com/" /> Production
 </div>
 <div class="environment">
-    <input type="radio" <?= $stgchecked; ?> name="<?= $this->plugin_name; ?>-env_baseurl" value="https://api.print.beer/" /> Staging
+    <input type="radio" <?= $stgchecked; ?> name="<?= $this->plugin_name; ?>-env_baseurl" value="https://api.stg.print.com/" /> Staging
 </div>
