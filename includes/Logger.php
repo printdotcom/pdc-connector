0a2c1373d49115897d57e072309dc14be21712e4
tijmen <tijmenbruggeman@gmail.com>
2026-06-16 21:37:47 +0200

    add actual api client env to logger

--- includes/Logger.php
+++ includes/Logger.php
@@ -12,6 +12,8 @@
 
 namespace PdcPod\Includes;
 
+use PdcPod\Admin\PrintDotCom\APIClient;
+
 class Logger {
 
 
@@ -119,12 +121,13 @@ class Logger {
 	private function get_system_info() {
 		global $wp_version;
 		$theme = wp_get_theme();
+		$apiclient = new APIClient();
 
 		$info  = "=== System Information ===\n";
 		$info .= 'WordPress Version: ' . $wp_version . "\n";
 		$info .= 'PHP Version: ' . phpversion() . "\n";
 		$info .= 'Plugin Version: ' . PDC_POD_VERSION . "\n";
-		$info .= 'Print.com Environment: ' . get_option( PDC_POD_NAME . '-env' ) . "\n";
+		$info .= 'Print.com Environment: ' . $apiclient->get_api_base_url() . "\n";
 		$info .= 'Server Software: ' . $_SERVER['SERVER_SOFTWARE'] . "\n";
 		$info .= 'Active Theme: ' . $theme->get( 'Name' ) . ' (' . $theme->get( 'Version' ) . ")\n";
 		$info .= 'Multisite: ' . ( is_multisite() ? 'Yes' : 'No' ) . "\n";
